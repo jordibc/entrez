576c08703f59be530b639ada1df4ed824094149c
Jordi <jordi.burguet.castell@gmail.com>
2015-08-18 00:31:18 -0700

    initial commit

---
+++ acc2gi.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+"""
+Use Entrez utilities to get the GIs of the given accession
+numbers, or the ones that can be extracted from some fasta files.
+"""
+
+import re
+import argparse
+
+import entrez
+
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-f', '--fastas', metavar='FILE', nargs='+',
+                       help='fasta files with the accession numbers')
+    group.add_argument('-a', '--accessions', metavar='ACC', nargs='+',
+                       help='accession numbers')
+    parser.add_argument('-n', '--nreq', type=int, default=100,
+                        help='number of accession numbers per request')
+    parser.add_argument('-m', '--max', type=int, default=-1,
+                        help='maximum number to query')
+    args = parser.parse_args()
+
+    # Get accessions from the given list, or from the given files.
+    accessions = (args.accessions if args.accessions
+                  else read_accessions(args.fastas))
+
+    # Print the GIs and accession numbers for all accession numbers,
+    # in groups of args.nreq numbers per request to the NCBI webservers.
+    for i in range(0, len(accessions), args.nreq):
+        print_acc2gi(accessions[i:i+args.nreq])
+        if i >= args.max >= 0:
+            break
+
+
+
+def print_acc2gi(accessions):
+    """Print GIs corresponding to the given accession numbers."""
+    term = ' OR '.join(a + '[accn]' for a in accessions)
+    for line in entrez.eapply(db='nucleotide', term=term, tool='summary'):
+        if 'Name="Extra"' in line and any(a in line for a in accessions):
+            _, gi, _, acc, _ = line.split('|', 4)
+            print('%18s  ->  %s' % (acc, gi))
+
+
+def read_accessions(fnames):
+    """Return a list of accession numbers from a list of fasta files."""
+    accessions = []
+    for fname in fnames:
+        for line in open(fname):
+            if line.startswith('>'):  # header with the accession number info
+                raw = line.strip('> \n').split(' ', 1)[0]  # take the beginning
+                accessions.append(parse(raw))
+    return accessions
+
+
+def parse(raw):
+    """Return the accession number contained in the raw string of a fasta."""
+    # A quite sui-generis parser to get the access numbers.
+
+    # For example, with the given inputs on the left, it returns...
+    #   X64695.1.gene9                 ->  X64695.1
+    #   VanY-D_4_AY489045              ->  AY489045
+    #   2:1314_M29695.1                ->  M29695.1
+    #   (Tmt)DfrB4:FM87748469-305:237  ->  FM87748469
+
+    if re.search('NC_\d+', raw):            # Eg: NC_013773
+        return re.search('NC_\d+', raw).group()
+    elif re.search('NZ_[A-Z0-9]+', raw):    # Eg: NZ_AGSO01000004.1
+        return re.search('NZ_[A-Z0-9]+', raw).group()
+    elif '_' in raw:             # Eg: VanY-D_4_AY489045, dfrB3_1_FM877478
+        return raw.split('_')[-1]
+    elif re.search('.orf\d*.gene$', raw):   # Eg: EU177504.2.orf0.gene
+        return raw.split('.orf')[0]
+    elif re.search('.gene\d*$', raw):       # Eg: AY139592.1.gene4
+        return raw.split('.gene')[0]
+    elif ':' in raw:                        # Eg: (Tmt)DfrB4:FM87748469-305:237
+        return raw.split(':')[1].split('-')[0]
+    else:
+        raise RuntimeError('Do not know how to parse: %s' % raw)
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ entrez.py
@@ -0,0 +1,94 @@
+"""Simple interface to the amazing NCBI databases (Entrez).
+
+equery(tool, ...) - Return the http response of a query.
+eapply(db, term, tool[, db2, retmax, ...]) - Yield the output of
+                    applying a tool over the results of a query.
+
+Examples of use:
+
+* Fetch information for SNP with id 3000, as in the example of
+  http://www.ncbi.nlm.nih.gov/projects/SNP/SNPeutils.htm
+
+  for line in equery(tool='fetch', db='snp', id='3000'):
+      print(line.rstrip())
+
+* Get a summary of nucleotides related to accession numbers
+  NC_010611.1 and EU477409.1
+
+  for line in eapply(db='nucleotide',
+                     term='NC_010611.1[accs] OR EU477409.1[accs]',
+                     tool='summary'):
+      print(line.rstrip())
+
+"""
+
+#
+# Useful references (at http://www.ncbi.nlm.nih.gov/books):
+# * Converting accession numbers:
+#     /NBK25498/#chapter3.Application_2_Converting_access
+# * Retrieving large datasets:
+#     /NBK25498/#chapter3.Application_3_Retrieving_large
+# * E-Utilities:
+#     /NBK25497/#chapter2.The_Nine_Eutilities_in_Brief
+#
+
+import re
+try:
+    from urllib import urlencode
+    from urllib2 import urlopen
+except ImportError:  # Python 3
+    from urllib.parse import urlencode
+    from urllib.request import urlopen
+
+
+_valid_tools = 'info search post summary fetch link gquery citmatch'.split()
+_valid_params = ('db term id usehistory query_key WebEnv '
+                 'rettype retmode retstart retmax').split()
+
+
+def equery(tool='search', **params):
+    """Return http response for the requested E-utility query."""
+    # Do a POST request to the desired Entrez tool.
+
+    # First make some basic checks.
+    assert tool in _valid_tools, 'Invalid web tool: %s' % tool
+    for k in params:
+        assert k in _valid_params, 'Unknown parameter: %s' % k
+    # TODO: we could really check better than this...
+
+    # Make a POST request and return the http response object.
+    url = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/e%s.fcgi' % tool
+    return urlopen(url, urlencode(params).encode('ascii'))
+    # TODO: handle errors gracefully.
+
+
+def eapply(db, term, tool, db2=None, retmax=500, **params):
+    """Yield the results of querying db with term, and using tool over them.
+
+    Select (search) the elements in database db that satisfy the query
+    in term, and yield the output of applying the given tool on them.
+
+    Args:
+      db: Database where the query is done.
+      term: Query term that selects elements to process later.
+      tool: E-Utilitiy that is used on the selected elements.
+      db2: Database where tool is applied. If None, it's the same as db.
+      retmax: Chunk size of the reading from the NCBI servers.
+      params: Extra parameters to use with the E-Utility.
+    """
+    # Use the search tool with usehistory='y' to select the elements.
+    output = equery(tool='search', usehistory='y',
+                    db=db, term=term).read().decode('ascii')
+
+    web = re.search('<WebEnv>(\S+)</WebEnv>', output).groups()[0]
+    key = re.search('<QueryKey>(\d+)</QueryKey>', output).groups()[0]
+    count_match = re.search('<Count>(\d+)</Count>', output)
+    count = int(count_match.groups()[0]) if count_match else 1
+    # Now we can use web and key to reference the results of the search.
+
+    # Ask for the results of using tool over the selected elements, in
+    # batches of retmax each.
+    for retstart in range(0, count, retmax):
+        for line in equery(tool=tool, db=(db2 or db), query_key=key, WebEnv=web,
+                           retstart=retstart, retmax=retmax, **params):
+            yield line.decode('ascii')
